89980eb87fd20e61035a56699b3019c99d6b602a
stephanygrace <stephanytayong73@gmail.com>
2023-03-13 14:09:47 +0800

    add create-react-app

--- reactjs-php/src/App.js
+++ reactjs-php/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import $ from "jquery";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState("");
+	const [result, setResult] = useState("");
+
+	const handleChange = (e) => {
+		setName(e.target.value);
+	};
+
+	const handleSumbit = (e) => {
+		e.preventDefault();
+		const form = $(e.target);
+		$.ajax({
+			type: "POST",
+			url: form.attr("action"),
+			data: form.serialize(),
+			success(data) {
+				setResult(data);
+			},
+		});
+	};
+
+	return (
+		<div className="App">
+			<form
+				action="http://localhost:3001/server.php"
+				method="post"
+				onSubmit={(event) => handleSumbit(event)}
+			>
+				<label htmlFor="name">Name: </label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					value={name}
+					onChange={(event) => handleChange(event)}
+				/>
+				<br />
+				<button type="submit">Submit</button>
+			</form>
+			<h1>{result}</h1>
+		</div>
+	);
 }
 
 export default App;
